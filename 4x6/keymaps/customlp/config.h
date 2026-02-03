233e3156561b1c5164eaa5ba7230fd65c9bbc0ac
gglapoule <elie973@gmail.com>
2026-02-03 23:24:15 +0100

    remove auto pointer layer

--- 4x6/keymaps/customlp/config.h
+++ 4x6/keymaps/customlp/config.h
@@ -1,4 +1,5 @@
-// Auto pointer layer - activates LAYER_POINTER when trackball is used
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 5  // your LAYER_POINTER index
-#define AUTO_MOUSE_TIME 650         // timeout in ms (optional, default 650)
+// // Auto pointer layer - activates LAYER_POINTER when trackball is used
+// #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+// #define AUTO_MOUSE_DEFAULT_LAYER 5  // your LAYER_POINTER index
+// #define AUTO_MOUSE_TIME 650         // timeout in ms (optional, default 650)
+// #define AUTO_MOUSE_THRESHOLD 10     // threshold in steps (optional, default 10)
--- 4x6/keymaps/customlp/keymap.c
+++ 4x6/keymaps/customlp/keymap.c
@@ -16,6 +16,16 @@
  */
 #include QMK_KEYBOARD_H
 
+// Combo definitions
+const uint16_t PROGMEM combo_copy[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM combo_paste[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM combo_cut[] = {KC_X, KC_V, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(combo_copy, C(KC_C)),   // X + C = Copy
+    COMBO(combo_paste, C(KC_V)),  // C + V = Paste
+    COMBO(combo_cut, C(KC_X)),    // X + V = Cut
+};
 
 /* Charybdis-specific features. */
 #define CHARYBDIS_DRAGSCROLL_REVERSE_X
@@ -48,9 +58,17 @@ enum charybdis_keymap_layers {
 #define NUM_BSPC LT(LAYER_NUM, KC_BSPC)
 #define FUN_DEL  LT(LAYER_FUN, KC_DEL)
 
+
 // Pointer layer tap
 #define PT_Z     LT(LAYER_POINTER, KC_Z)
-#define PT_SLSH  LT(LAYER_POINTER, KC_SLSH)
+#define PT_SLSH     LT(LAYER_POINTER, KC_SLSH)
+
+// Custom keycodes for dragscroll/sniping tap-hold
+enum custom_keycodes {
+    DRG_ESC = SAFE_RANGE,
+    SNP_TAB,
+};
+
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -60,12 +78,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_ESC,    HM_A,    HM_S,    HM_D,    HM_F,    KC_G,       KC_H,    HM_J,    HM_K,    HM_L, HM_SCLN, KC_QUOT,
+        SNIPING,    HM_A,    HM_S,    HM_D,    HM_F,    KC_G,       KC_H,    HM_J,    HM_K,    HM_L, HM_SCLN, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LSFT,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH,  KC_ENT,
+       DRGSCRL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH,  KC_ENT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_ESC,  NAV_SPC,  KC_TAB,    SYM_ENT, NUM_BSPC,
-                                           KC_LCTL, KC_LSFT,    FUN_DEL
+                                  DRG_ESC,  NAV_SPC, SNP_TAB,    SYM_ENT, NUM_BSPC,
+                                           MS_BTN1, KC_LSFT,    FUN_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -75,12 +93,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_AGIN,S(KC_INS),C(KC_INS),S(KC_DEL),KC_UNDO,XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, XXXXXXX,    KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, CW_TOGG, XXXXXXX,
+       SNIPING, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, XXXXXXX,    KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, CW_TOGG, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,    KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_INS, XXXXXXX,
+       DRGSCRL, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,    KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_INS, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   XXXXXXX, XXXXXXX, XXXXXXX,     KC_ENT, KC_BSPC,
-                                           KC_LCTL, KC_LSFT,     KC_DEL
+                                           MS_BTN1, KC_LSFT,     KC_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -90,12 +108,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, KC_PLUS,    KC_4,    KC_5,    KC_6,  KC_EQL,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_RALT, XXXXXXX,
+       SNIPING, KC_PLUS,    KC_4,    KC_5,    KC_6,  KC_EQL,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_RALT, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX,  KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
+       DRGSCRL,  KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                    KC_DOT,    KC_0, KC_MINS,    XXXXXXX, XXXXXXX,
-                                           KC_LCTL, KC_LSFT,    XXXXXXX
+                                           MS_BTN1, KC_LSFT,    XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -105,97 +123,106 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX,   KC_LT,  KC_DLR, KC_PERC, KC_CIRC,   KC_GT,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_RALT, XXXXXXX,
+       SNIPING,   KC_LT,  KC_DLR, KC_PERC, KC_CIRC,   KC_GT,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_RALT, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
+       DRGSCRL, KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   KC_LPRN, KC_RPRN, KC_UNDS,    XXXXXXX, XXXXXXX,
-                                           KC_LCTL, KC_LSFT,    XXXXXXX
+                                           MS_BTN1, KC_LSFT,    XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
   [LAYER_FUN] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, XXXXXXX,  KC_F10,  KC_F11, KC_F12,  XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX,  KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX,
+       XXXXXXX,  XXXXXXX, KC_F7,   KC_F8,   KC_F9, KC_PSCR,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX,  KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SCRL,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_RALT, XXXXXXX,
+       SNIPING,  XXXXXXX, KC_F4,   KC_F5,   KC_F6, KC_SCRL,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_RALT, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX,  KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
+       DRGSCRL,  XXXXXXX, KC_F1,   KC_F2,   KC_F3, KC_PAUS,    XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                    KC_APP,  KC_SPC,  KC_TAB,    XXXXXXX, XXXXXXX,
-                                           KC_LCTL, KC_LSFT,    XXXXXXX
+                                           MS_BTN1, KC_LSFT,    XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
-  [LAYER_POINTER] = LAYOUT(
+    [LAYER_POINTER] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SNIPING, SNP_TOG,    DRG_TOG, DRGSCRL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, XXXXXXX,   S_D_MOD, KC_RSFT, KC_RCTL, KC_RGUI, KC_RALT, XXXXXXX,
+        SNIPING,    HM_A,    HM_S,    HM_D,    HM_F,    KC_G,    S_D_MOD, DPI_RMOD, DPI_MOD, S_D_RMOD, HM_SCLN, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, _______, XXXXXXX, XXXXXXX, EE_CLR,  QK_BOOT,    DPI_MOD, EE_CLR,  XXXXXXX, XXXXXXX, _______, XXXXXXX,
+       DRGSCRL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,  MS_BTN1, MS_BTN3,  MS_BTN2, KC_SLSH, KC_ENT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  MS_BTN1, MS_BTN3, MS_BTN2,    XXXXXXX, XXXXXXX,
-                                           DRGSCRL, XXXXXXX,    XXXXXXX
+                                  KC_ESC,  NAV_SPC,  KC_TAB,    SYM_ENT, NUM_BSPC,
+                                           MS_BTN1, KC_LSFT,    FUN_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
+//   [LAYER_POINTER] = LAYOUT(
+//   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+//        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+//        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SNP_TOG,    DRG_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+//        XXXXXXX, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, XXXXXXX,   S_D_MOD, DPI_RMOD, DPI_MOD, S_D_RMOD, SNIPING, XXXXXXX,
+//   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+//        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+//   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+//                                   MS_BTN1, MS_BTN3, MS_BTN2,    XXXXXXX, XXXXXXX,
+//                                            DRGSCRL, XXXXXXX,    XXXXXXX
+//   //                            ╰───────────────────────────╯ ╰──────────────────╯
+//   ),
 };
 
-void pointing_device_init_user(void) {
-    set_auto_mouse_layer(LAYER_POINTER);
-    set_auto_mouse_enable(true);
-}
+// void pointing_device_init_user(void) {
+//     set_auto_mouse_layer(LAYER_POINTER);
+//     set_auto_mouse_enable(true);
+// }
 
-// Track mouse button state to persist across layer changes
-static uint8_t mouse_buttons_state = 0;
+// bool is_mouse_record_user(uint16_t keycode, keyrecord_t* record) {
+//     switch (keycode) {
+//         case DRGSCRL:
+//         case DRG_TOG:
+//         case SNIPING:
+//         case SNP_TOG:
+//         case DPI_MOD:
+//         case S_D_MOD:
+//             return true;
+//         default:
+//             return false;
+//     }
+// }
 
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    // Restore button state that might have been cleared by layer change
-    mouse_report.buttons |= mouse_buttons_state;
-    return mouse_report;
-}
+static uint16_t drg_esc_timer;
+static uint16_t snp_tab_timer;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case MS_BTN1:
+        case DRG_ESC:
             if (record->event.pressed) {
-                mouse_buttons_state |= (1 << 0);
+                drg_esc_timer = timer_read();
+                charybdis_set_pointer_dragscroll_enabled(true);
             } else {
-                mouse_buttons_state &= ~(1 << 0);
+                charybdis_set_pointer_dragscroll_enabled(false);
+                if (timer_elapsed(drg_esc_timer) < TAPPING_TERM) {
+                    tap_code(KC_ESC);
+                }
             }
-            break;
-        case MS_BTN2:
+            return false;
+        case SNP_TAB:
             if (record->event.pressed) {
-                mouse_buttons_state |= (1 << 1);
+                snp_tab_timer = timer_read();
+                charybdis_set_pointer_sniping_enabled(true);
             } else {
-                mouse_buttons_state &= ~(1 << 1);
+                charybdis_set_pointer_sniping_enabled(false);
+                if (timer_elapsed(snp_tab_timer) < TAPPING_TERM) {
+                    tap_code(KC_TAB);
+                }
             }
-            break;
-        case MS_BTN3:
-            if (record->event.pressed) {
-                mouse_buttons_state |= (1 << 2);
-            } else {
-                mouse_buttons_state &= ~(1 << 2);
-            }
-            break;
-    }
-    return true;
-}
-
-bool is_mouse_record_user(uint16_t keycode, keyrecord_t* record) {
-    switch (keycode) {
-        case DRGSCRL:
-        case DRG_TOG:
-        case SNIPING:
-        case SNP_TOG:
-        case DPI_MOD:
-        case S_D_MOD:
-            return true;
-        default:
             return false;
     }
+    return true;
 }
